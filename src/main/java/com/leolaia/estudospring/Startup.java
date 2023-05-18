1bffb9d22f125c09a750aad620d46840142de7c9
Leo Laia <leonardolribo@gmail.com>
2023-05-18 15:20:55 -0300

    Ajustes iniciais

--- src/main/java/com/leolaia/estudospring/Startup.java
+++ src/main/java/com/leolaia/estudospring/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EstudoSpringApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EstudoSpringApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }

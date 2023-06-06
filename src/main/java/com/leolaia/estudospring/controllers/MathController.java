498dd1e4a3c1fad99e29b86935ba41fc64e3dff1
Leo Laia <leonardolribo@gmail.com>
2023-06-06 10:51:30 -0300

    Implementado Serviço para soma e tratamento de exceções

---
+++ src/main/java/com/leolaia/estudospring/controllers/MathController.java
@@ -0,0 +1,31 @@
+package com.leolaia.estudospring.controllers;
+
+import com.leolaia.estudospring.exceptions.UnsuportedMathOperationException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MathController {
+
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double sum(
+            @PathVariable(value = "numberOne") String numberOne,
+            @PathVariable(value = "numberTwo") String numberTwo
+    ) throws Exception{
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new UnsuportedMathOperationException("Please set a numeric value");
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) return 0D;
+        String number = strNumber.replaceAll(",", ".");
+        if(isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[+-]?[0-9]*\\.?[0-9]+");
+    }
+}
---
+++ src/main/java/com/leolaia/estudospring/exceptions/ExceptionResponse.java
@@ -0,0 +1,31 @@
+package com.leolaia.estudospring.exceptions;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class ExceptionResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Date date;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date date, String message, String details) {
+        this.date = date;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}

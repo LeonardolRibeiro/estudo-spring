8fa6bdc2dcc14378996772f852cd84abe021ebad
Leo Laia <leonardolribo@gmail.com>
2023-06-12 16:33:37 -0300

    Criado versao inicial do CRUD

--- src/main/java/com/leolaia/estudospring/controllers/PersonController.java
+++ src/main/java/com/leolaia/estudospring/controllers/PersonController.java
@@ -4,10 +4,9 @@ import com.leolaia.estudospring.models.Person;
 import com.leolaia.estudospring.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -16,8 +15,28 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll(){
+        return personService.findAll();
+    }
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) throws Exception{
         return personService.findById();
     }
+
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) throws Exception{
+        return personService.create(person);
+    }
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) throws Exception{
+        return personService.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable(value = "id") String id) throws Exception{
+        personService.delete();
+    }
+
+
 }
--- src/main/java/com/leolaia/estudospring/services/PersonService.java
+++ src/main/java/com/leolaia/estudospring/services/PersonService.java
@@ -3,6 +3,8 @@ package com.leolaia.estudospring.services;
 import com.leolaia.estudospring.models.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +15,15 @@ public class PersonService {
     private Logger logger = Logger.getLogger(PersonService.class.getName());
 
 
+    public List<Person> findAll(){
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
     public Person findById(){
         logger.info("Finding one person!");
         Person person = new Person();
@@ -23,4 +34,32 @@ public class PersonService {
         person.setGender("Male");
         return person;
     }
+
+    public Person create(Person person) {
+        logger.info("Creating one person!");
+
+        return person;
+    }
+
+    public Person update(Person person) {
+        logger.info("Updating one person!");
+
+        return person;
+    }
+
+    public void delete() {
+        logger.info("Deleting one person!");
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brazil " + i);
+        person.setGender("Male");
+        return person;
+    }
+
+
 }

a1954bceacb731ce37e9a4b9206586d8f4006aec
Leo Laia <leonardolribo@gmail.com>
2023-06-14 17:36:24 -0300

    Adicionado suporte a migrations com Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `first_name`, `last_name`, `address`, `gender`) VALUES
+(1, 'Leonardo', 'Laia', 'Rua qualquer - Colombo - Paraná', 'Male'),
+(2, 'Mayara', 'Antunes', 'Rua qualquer - Colombo - Paraná', 'Female'),
+(3, 'Gustavo', 'Laia', 'Rua qualquer - Colombo - Paraná', 'Male'),
+(4, 'Vera', 'Antunes', 'Rua qualquer - Colombo - Paraná', 'Female');
+

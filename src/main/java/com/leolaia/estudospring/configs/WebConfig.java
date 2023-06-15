c662e616b6b3ae7c6962f73d6cee72e8b30681f0
Leo Laia <leonardolribo@gmail.com>
2023-06-15 11:37:23 -0300

    Adicionado suporte ao CONTENT NEGOTIATION via header param

--- src/main/java/com/leolaia/estudospring/configs/WebConfig.java
+++ src/main/java/com/leolaia/estudospring/configs/WebConfig.java
@@ -11,8 +11,18 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
         //VIA QUERY PARAM: http://localhost:8080/api/person/v1?mediaType=xml
-        configurer.favorParameter(true)
+        /*configurer.favorParameter(true)
                 .parameterName("mediaType").ignoreAcceptHeader(true)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                //formatos suportados
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);*/
+
+        //VIA HEADER PARAM: http://localhost:8080/api/person/v1
+        // O nome do parâmetro via header é Accept e o valor deve ser: application/xml ou application/json
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 //formatos suportados

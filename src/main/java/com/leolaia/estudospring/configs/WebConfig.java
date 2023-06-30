eb6e35166c52914305dd7837645350231aef4981
Leo Laia <leonardolribo@gmail.com>
2023-06-30 10:20:18 -0300

    Habilitando CORS de forma global

--- src/main/java/com/leolaia/estudospring/configs/WebConfig.java
+++ src/main/java/com/leolaia/estudospring/configs/WebConfig.java
@@ -1,10 +1,12 @@
 package com.leolaia.estudospring.configs;
 
 import com.leolaia.estudospring.serialization.converters.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,6 +15,8 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+    @Value("${cors.originPatterns:default}") // Essa anotação faz o spring procurar essa propriedade no arquivo application.properties e inicializar o valor
+    private String corsOriginPatterns = "";
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -31,5 +35,16 @@ public class WebConfig implements WebMvcConfigurer {
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
                 .mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        //Se atentar para não passar ' ' (espaços) entre as origens permitidas no arquivo application.properties
+
+        registry.addMapping("/**") //Liberamos acesso a todos os serviços de nossa api
+            .allowedMethods("*")//.allowedMethods("GET", "POST", "PUT")
+            .allowedOrigins(allowedOrigins)
+            .allowCredentials(true);
     }
 }

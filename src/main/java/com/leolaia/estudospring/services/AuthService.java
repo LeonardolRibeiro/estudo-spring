b220232ffa430f2969b11121c4dfea7433c72535
Leo Laia <leonardolribo@gmail.com>
2023-06-30 17:52:33 -0300

    Implementado lógica do AuthService
    aqui ligamos todos os pontos que desenvolvemos antes

---
+++ src/main/java/com/leolaia/estudospring/services/AuthService.java
@@ -0,0 +1,48 @@
+package com.leolaia.estudospring.services;
+
+import com.leolaia.estudospring.data.vo.v1.security.AccountCredentialsVO;
+import com.leolaia.estudospring.data.vo.v1.security.TokenVO;
+import com.leolaia.estudospring.repositories.UserRepository;
+import com.leolaia.estudospring.security.jwt.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
+
+    public ResponseEntity signin(AccountCredentialsVO data){
+        try{
+            var username = data.getUserName();
+            var password = data.getPassword();
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = repository.findByUsername(username);
+
+            var tokenResponse = new TokenVO();
+
+            if (tokenResponse != null){
+                tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            }else {
+                throw new UsernameNotFoundException("Username " + username + " not found");
+            }
+
+            return ResponseEntity.ok(tokenResponse);
+        }catch (Exception e){
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+}
